2fe43abfa3ae28b7c0e1dda9dbe5e657a6867281
od-c0d3r <omar_rashad@aol.com>
2022-01-10 11:35:54 +0200

    Code: define Header, TodoContainer components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.scss';
+
+function Header() {
+  return (
+    <header>
+      header
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/TodoContainer/TodoContainer.js
@@ -0,0 +1,13 @@
+import './TodoContainer.scss';
+import Header from '../Header/Header'
+
+function TodoContainer() {
+  return (
+    <div id="todoContainer">
+      #todoContainer
+      <Header />
+    </div>
+  );
+}
+
+export default TodoContainer;
