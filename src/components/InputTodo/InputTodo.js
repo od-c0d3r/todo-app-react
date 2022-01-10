60afe55ee7cd6a26bb4426ddffcf769560ce64ae
od-c0d3r <omar_rashad@aol.com>
2022-01-10 11:40:29 +0200

    Code: structure InputTodo

---
+++ src/components/InputTodo/InputTodo.js
@@ -0,0 +1,11 @@
+import './InputTodo.scss';
+
+function App() {
+  return (
+    <div id='todoInput' >
+      #todoInput
+    </div>
+  );
+}
+
+export default App;
--- src/components/TodoContainer/TodoContainer.js
+++ src/components/TodoContainer/TodoContainer.js
@@ -1,11 +1,13 @@
 import './TodoContainer.scss';
-import Header from '../Header/Header'
+import Header from '../Header/Header.js'
+import InputTodo from '../InputTodo/InputTodo.js'
 
 function TodoContainer() {
   return (
     <div id="todoContainer">
       #todoContainer
       <Header />
+      <InputTodo />
     </div>
   );
 }
