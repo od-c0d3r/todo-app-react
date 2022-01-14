1f47b8eb8c19f4680a711e83b5ec285884092695
Omar R <omar_rashad@aol.com>
2022-01-14 16:40:25 +0200

    Fix: code indentation

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,13 @@
 import Navbar from '../Navbar/Navbar';
-import './About.scss'
+import './About.scss';
 
 function About() {
-    return (
-        <div id="aboutPage">
-            <Navbar />
-            <span>This is about page</span>
-        </div>
-    );
+  return (
+    <div id="aboutPage"
+      <Navbar />
+        <span>This is about page</span>
+      </div>
+  );
 };
 
 export default About;
